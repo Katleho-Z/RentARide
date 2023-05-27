168005e52e194e99fb01e4c113094e103d87ef08
Mzi Mthethwa <mzimkhulu125@gmail.com>
2023-05-27 15:25:41 +0200

    Index Page-fixing banner

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ user2 = User.create!(name: "Jane", email: "jane@example.com", password: "passwor
   user1.cars.create!(
     make: "Toyota",
     model: "Camry",
+    image_url: "https://img-ik.cars.co.za/news-site-za/images/2019/06/Toyota-Camry-4.jpg",
     price_per_day: rand(30..100).to_f
   )
 end
@@ -30,9 +31,18 @@ end
   user2.cars.create!(
     make: "Honda",
     model: "Civic",
+    image_url: "https://www.thedrive.com/content/2021/12/HERO.jpg?quality=85&crop=16%3A9&auto=webp&optimize=high&quality=70&width=1440",
     price_per_day: rand(30..100).to_f
   )
 end
 
+2.times do
+  user2.cars.create!(
+    make: "Haval",
+    model: "H6",
+    image_url: "https://res.cloudinary.com/driveau/image/upload/v1638932222/cms/uploads/kiltbyygqepvk0h8o3dd.jpg",
+    price_per_day: rand(30..100).to_f
+  )
+end
 puts "#{User.count} users created"
 puts "#{Car.count} cars created"
