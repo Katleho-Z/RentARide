0f59492e5d7d4e97feb5e9074adf292eb432fda8
Mzi Mthethwa <mzimkhulu125@gmail.com>
2023-06-01 19:49:02 +0200

    Index Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  #root to: "pages#home"
+  root to: "cars#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"\
-
   resources :cars do
     resources :bookings, only: [:new, :create]
   end
 
+  get '/search_cars', to: 'cars#search', as: 'search_cars'
+  get '/bookings', to: 'bookings#index', as: 'bookings'
+
+
   resources :bookings, only: [:index, :show]
+
 end
